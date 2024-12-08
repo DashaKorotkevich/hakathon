d93a8d40b544d1b507110d00c748293e8a2a8f84
DashaKorotkevich <dashakorotkevich6878@gmail.com>
2024-12-08 12:09:57 +0300

    add: cameras

--- script.js
+++ script.js
@@ -89,6 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
   }
 
+// Поиск
 
 const poiskInput = document.getElementById('table-poisk-input');
 const submitPoisk = document.getElementById('table-submit-poisk');
@@ -98,11 +99,18 @@ submitPoisk.onclick = function(event) {
   const userInputValue = poiskInput.value;
 
   const foundObject = arrRoad.find(obj => obj.name === userInputValue);
-  if (foundObject) {
+  if(userInputValue==="") {
+    poiskInput.placeholder = 'Вы не ввели улицу'
+    poiskInput.style.color="red"
+  }
+  if (foundObject) { 
+    const name = document.getElementById("table-name");
     const numberOfCars = document.getElementById("table-info-number-of-cars");
     const speed = document.getElementById("table-info-speed");
     const density = document.getElementById("table-info-density");
     const accident = document.getElementById("table-info-accident");
+    name.innerHTML = `${
+      foundObject.name}`
     numberOfCars.innerHTML = `${
       foundObject.numberOfCars}`
       speed.innerHTML = `${
@@ -124,12 +132,165 @@ submitPoisk.onclick = function(event) {
       };
 }
 
+// Клик по светофорам
+const  tableOpenTrafficLights= document.getElementById('table-open-traffic-lights');
 
+tableOpenTrafficLights.onclick = function(event) {
+  event.preventDefault();
+  const arrTrafficLights=[
+    { 
+      id: 0,
+      group: 1,
+      x: 45.040183,
+      y: 38.976389,
+      xAnchor: 10.040183,
+      yAnchor: 27.976389,
+      modeOld: 1,
+      timeOfChange: "12:30",
+      modeNow: 1,
+    },
+    { 
+      id: 1,
+      group: 1,
+      x: 45.037052,
+      y: 38.975166,
+      xAnchor: 10.040183,
+      yAnchor: 27.976389,
+      modeOld: 1,
+      timeOfChange: "12:00",
+      modeNow: 1,
+    },
+    {
+      id: 2,
+      group: 1,
+      x: 45.042204,
+      y: 38.977180,
+      xAnchor: 10.040183,
+      yAnchor: 27.976389,
+      modeOld: 1,
+      timeOfChange: "11:00",
+      modeNow: 1,
+    },
+    { 
+      id: 3,
+      group: 2,
+      x: 45.036782,
+      y: 38.976689,
+      xAnchor: 10.040183,
+      yAnchor: 27.976389,
+      modeOld: 1,
+      timeOfChange: "12:01",
+      modeNow: 1,
+    },
+    { 
+      id: 4,
+      group: 2,
+      x: 45.040183,
+      y: 38.976389,
+      xAnchor: 10.040183,
+      yAnchor: 27.976389,
+      modeOld: 1,
+      timeOfChange: "12:30",
+      modeNow: 1,
+    },
+    { 
+      id: 5,
+      group: 2,
+      x: 45.036400,
+      y:  38.978846,
+      xAnchor: 10.040183,
+      yAnchor: 27.976389,
+      modeOld: 1,
+      timeOfChange: "12:40",
+      modeNow: 1,
+    },
 
+    { 
+      id: 6,
+      group: 3,
+      x: 45.039607,
+      y: 38.980037,
+      xAnchor: 10.040183,
+      yAnchor: 27.976389,
+      modeOld: 1,
+      timeOfChange: "12:50",
+      modeNow: 1,
+    },
+    { 
+      id: 7,
+      group: 3,
+      x: 45.038895,
+      y: 38.984311,
+      xAnchor: 10.040183,
+      yAnchor: 27.976389,
+      modeOld: 1,
+      timeOfChange: "14:00",
+      modeNow: 1,
+    }
+  ]
+  arrTrafficLights.forEach((item)=>{
+    var customIcon = L.icon({
+      iconUrl: './icon/traffic_light.svg', // Путь к вашей иконке
+      iconSize: [25, 25], // Размер иконки
+      iconAnchor: [10.040183, 27.976389], // Анкорная точка иконки
+      popupAnchor: [0, -50] // Анкорная точка для попапа
+    });
+
+    // Создание метки
+  
+    let marker = L.marker([item.x,item.y ],{ icon: customIcon }).addTo(map);
+  
+    // Добавление всплывающего окна к метке
+    marker.bindPopup(`<p>Предыдущий режим: ${item.modeOld}</p><p>Время изменений: ${item.timeOfChange}</p><b>Текущий режим: ${item.modeNow}</b>`).openPopup();
+
+  })
+}
+
+//Клик по камеарм
+
+const  tableOpenCameras= document.getElementById('table-opencameras');
+const  closeCamera= document.getElementById('closeCamera');
+const  camerasContainer= document.getElementById('cameras-container');
+
+tableOpenCameras.onclick = function(event) {
+  event.preventDefault();
+  openModal(camera);
+
+    const video = document.createElement('video');
+        video.width = 330;
+        video.height = 235;
+        video.autoplay = true; // Автовоспроизведение
+        video.loop = true;    // Бесконечное повторение
+
+        const sourceMp4 = document.createElement('source');
+        sourceMp4.type = 'video/mp4';
+
+    const userInputValue = poiskInput.value;
+    if(userInputValue==='Красная'){
+        sourceMp4.src = './videos/Camera1.mp4';
+    }
+    else if(userInputValue==='Кузнечная'){
+        sourceMp4.src = './videos/Camera2.mp4';
+    }
+    else{
+        sourceMp4.src = './videos/Camera3.mp4';
+    }
+    const name = document.getElementById("camera-name");
+    name.innerHTML = `${
+          userInputValue}`
+    video.appendChild(sourceMp4);
+    camerasContainer.appendChild(video);
+}
+
+closeCamera.onclick = function(event) {
+  event.preventDefault();
+  camerasContainer.innerHTML = '';
+  closeModal(camera);
+};
 
 const arrRoad = [
   { id: 1,
-    name: "Ставропольская",
+    name: "Кузнечная",
     cameras: 1,
     numberOfCars: 20,
     speed: 60,
@@ -142,7 +303,7 @@ const arrRoad = [
     numberOfCars: 30,
     speed: 66,
     density: 1.5,
-    accident: false
+    accident: 'нет'
   },
   { id: 3,
     name: "Красная",
@@ -150,7 +311,7 @@ const arrRoad = [
     numberOfCars: 30,
     speed: 66,
     density: 1.5,
-    accident: false
+    accident: 'нет'
   },
 
 ]
